ec2c2142a4ae9a25ab8a7c503b7b56934371a3d2
Satyam <tsingh30399@gmail.com>
2024-01-21 00:26:36 +0530

    Stair Case

--- src/Mb_StairCase.java
+++ src/Mb_StairCase.java
@@ -1,2 +1,17 @@
 public class Mb_StairCase {
+    public static void main(String[] args) {
+        int n = 6;
+        for(int i = 0; i < n; i++){
+            for(int j = n-1; j > i; j--){
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= i; k++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
+
